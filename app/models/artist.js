a69f5a9b67a7de091f1e34450b1fcb01dc53452a
Steven Nunez <steven.nunez@gmail.com>
2015-11-16 18:46:46 -0500

    Setup

---
+++ app/models/artist.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  firstName: DS.attr('string'),
+  lastName: DS.attr('string'),
+  fullName: Ember.computed("firstName", "lastName", function(){
+    return `${this.get('firstName')} ${this.get('lastName')}`;
+  })
+});
